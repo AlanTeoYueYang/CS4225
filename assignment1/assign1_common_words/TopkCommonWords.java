9b898269de9b63b469cd71a66b5dbab6ad62b8dd
AlanTeoYueYang <47876194+AlanTeoYueYang@users.noreply.github.com>
2020-09-13 18:24:27 +0800

    Update TopkCommonWords.java

--- assignment1/assign1_common_words/TopkCommonWords.java
+++ assignment1/assign1_common_words/TopkCommonWords.java
@@ -1,271 +1,242 @@
-//assignment1 task1 commonwords java
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.net.URI;
+import java.util.StringTokenizer;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.StringTokenizer;
+
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.WritableComparable;
-import org.apache.hadoop.io.WritableComparator;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.util.GenericOptionsParser;
-import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
-
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
 
 public class TopkCommonWords {
 
-	public static class TokenizerWCMapper extends
-			Mapper<Object, Text, Text, IntWritable> {
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, IntWritable>{
 
-		Set<String> stopwords = new HashSet<String>();
-		private static final String STOP_WORD_PATH = "assignment1_task1/stopwords.txt";
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+        private Set stopWords = new HashSet();
 
-		@Override
-		protected void setup(Context context) {
-			try {
-				Path path = new Path(STOP_WORD_PATH);
-				FileSystem fs = FileSystem.get(new Configuration());
-				BufferedReader br = new BufferedReader(new InputStreamReader(
-						fs.open(path)));
-				String word = null;
-				while ((word = br.readLine()) != null) {
-					stopwords.add(word.toLowerCase());
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            URI[] cacheFiles = context.getCacheFiles();
+            if (cacheFiles != null && cacheFiles.length > 0) {
+                Path path = new Path(cacheFiles[0].toString());
+                readFile(path);
+            }
+        }
 
-		private final static IntWritable one = new IntWritable(1);
-		private Text word = new Text();
+        private void readFile(Path filePath) {
+            try{
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath.toString()));
+                String stopWord = null;
+                while((stopWord = bufferedReader.readLine()) != null) {
+                    stopWords.add(stopWord);
+                }
+            } catch(IOException ex) {
+                System.err.println("Exception while reading stop words file: " + ex.getMessage());
+            }
+        }
 
-		public void map(Object key, Text value, Context context)
-				throws IOException, InterruptedException {
-			StringTokenizer itr = new StringTokenizer(value.toString());
-			//delimiter: " \t\n\r\f"
-			while (itr.hasMoreTokens()) {
-				word.set(itr.nextToken());
-				if (stopwords.contains(word.toString().toLowerCase()))
-					continue;
-				context.write(word, one);
-			}
-		}
-	}
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                String curr_word = itr.nextToken();
 
-	public static class IntSumReducer extends
-			Reducer<Text, IntWritable, Text, IntWritable> {
-		private IntWritable count = new IntWritable();
+                curr_word = curr_word.replaceAll("\n","");
+                curr_word = curr_word.replaceAll("\t","");
+                curr_word = curr_word.replaceAll("\r","");
+                curr_word = curr_word.replaceAll("\f","");
+                if (!stopWords.contains(curr_word)){
+                    word.set(curr_word);
+                    context.write(word, one);
+                }
+            }
+        }
+    }
 
-		public void reduce(Text key, Iterable<IntWritable> values,
-						   Context context) throws IOException, InterruptedException {
-			int sum = 0;
-			for (IntWritable val : values) {
-				sum += val.get();
-			}
-			count.set(sum);
-			context.write(key, count);
-		}
-	}
+    public static class IntSumReducer
+            extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
 
-	//Mapper1
-	public static class Mapper1 extends Mapper<Text, Text, Text, Text> {
-		private Text frequency = new Text();
-		private Text word = new Text();
-		public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
-			//ToDo
-			//System.out.println("key: "+key+" value: "+value);
-			word.set(key);
-			frequency.set(value);
-			context.write(key, value);
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
 
-		}
-	}
+    public static class Mapper1 extends Mapper<Text, Text, Text, Text> {
+        private Text frequency = new Text();
+        private Text word = new Text();
+        public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
+            word.set(key);
+            frequency.set(value);
+            context.write(key, value);
 
-	//Mapper2
-	public static class Mapper2 extends Mapper<Text, Text, Text, Text> {
-		private Text frequency = new Text();
-		private Text word = new Text();
-		public void map(Text key, Text value, Context context)
-				throws IOException, InterruptedException {
-			//ToDo
-			word.set(key);
-			frequency.set(value);
-			context.write(key, value);
-		}
+        }
+    }
 
-	}
+    public static class Mapper2 extends Mapper<Text, Text, Text, Text> {
+        private Text frequency = new Text();
+        private Text word = new Text();
+        public void map(Text key, Text value, Context context)
+                throws IOException, InterruptedException {
+            word.set(key);
+            frequency.set(value);
+            context.write(key, value);
+        }
 
-	//Get the number of common words reduce
-	public static class CountCommonReducer extends
-			Reducer<Text, Text, Text, IntWritable> {
+    }
 
-		private Text word = new Text();
-		private IntWritable commoncount = new IntWritable();
+    public static class CommonWordsCount extends Reducer<Text, Text, Text, IntWritable> {
+        private Text word = new Text();
+        private IntWritable commoncount = new IntWritable();
 
-		public void reduce(Text key, Iterable<Text> values,
-						   Context context) throws IOException, InterruptedException {
+        public void reduce(Text key, Iterable<Text> values,
+                           Context context) throws IOException, InterruptedException {
+            int k = 0;
+            int frequency1 = 0;
+            int frequency2 = 0;
+            int commonFreq = 0;
+            for (Text value : values) {
+                k++;
+                if (k == 1) {
+                    frequency1 = Integer.parseInt(value.toString());
+                } else if (k == 2) {
+                    frequency2 = Integer.parseInt(value.toString());
+                }
+                if (k == 2) {
+                    commonFreq = Math.min(frequency1, frequency2);
+                    word.set(key);
+                    commoncount.set(commonFreq);
+                    context.write(word, commoncount);
+                }
+            }
+        }
+    }
 
-			//ToDo
-			int k = 0;
-			int frequency1 = 0;
-			int frequency2 = 0;
-			int commonFreq = 0;
-			for(Text value: values) {
-				k++;
-				if (k == 1) {
-					frequency1 = Integer.parseInt(value.toString());
-				}else if(k == 2) {
-					frequency2 = Integer.parseInt(value.toString());
-				}
-			}
-			if(k == 2) {
-				commonFreq = Math.min(frequency1, frequency2);
-				word.set(key);
-				commoncount.set(commonFreq);
-				context.write(word, commoncount);
-			}
+    public static class SortMapper extends Mapper<Text, Text, IntWritable, Text>{
+        private IntWritable count = new IntWritable();
+        private Text word = new Text();
 
-		}
+        public void map(Text key, Text value, Context context)
+                throws IOException, InterruptedException{
+            word.set(key);
+            count.set(Integer.parseInt(value.toString()));
+            context.write(count, word);
+        }
+    }
 
-	}
+    public static class SortReducer extends Reducer<IntWritable, Text, IntWritable, Text> {
+        private IntWritable count = new IntWritable();
+        private Text word = new Text();
+        public static final int top = 20;
+        public int size = 0;
 
-	//sort the result
-	public static class SortMapper extends Mapper<Text, Text, IntWritable, Text> {
-		private IntWritable count = new IntWritable();
-		private Text word = new Text();
+        public void reduce(IntWritable key, Iterable<Text> values,
+                           Context context) throws IOException, InterruptedException {
+            //ToDo
+            if(size < top) {
+                count.set(key.get());
+                for(Text value: values) {
+                    word.set(value.toString());
+                    context.write(count, word);
+                    size++;
+                    if(size >= top) {
+                        break;
+                    }
+                }
+            }
+        }
+    }
 
-		public void map(Text key, Text value, Context context)
-				throws IOException, InterruptedException {
-			//ToDo
-			word.set(key);
-			count.set(Integer.parseInt(value.toString()));
-			context.write(count, word);
-		}
-	}
+        //comparator --> descending sort
+    public static class IntKeyDescComparator extends WritableComparator{
+        protected IntKeyDescComparator() {
+            super(IntWritable.class, true);
+        }
 
-	public static class SortReducer extends
-			Reducer<IntWritable, Text, IntWritable, Text> {
-		private IntWritable count = new IntWritable();
-		private Text word = new Text();
-		public static final int NUM = 15;
-		public int size = 0;
+        @Override
+        public int compare(WritableComparable a, WritableComparable b) {
+            return -super.compare(a, b);
+        }
+    }
 
-		public void reduce(IntWritable key, Iterable<Text> values,
-						   Context context) throws IOException, InterruptedException {
-			//ToDo
-			if(size < NUM) {
-				count.set(key.get());
-				for(Text value: values) {
-					word.set(value.toString());
-					context.write(count, word);
-					size++;
-					if(size >= NUM) {
-						break;
-					}
-				}
-			}
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+        Job job1 = Job.getInstance(conf, "word count 1");
+        job1.setMapperClass(TokenizerMapper.class);
+        job1.setJarByClass(TopkCommonWords.class);
+        job1.setReducerClass(IntSumReducer.class);
+        job1.setOutputKeyClass(Text.class);
+        job1.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job1, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job1, new Path("temp_output1"));
+        job1.addCacheFile(new Path(args[2]).toUri());
+//        System.exit(job1.waitForCompletion(true) ? 0 : 1);
+        job1.waitForCompletion(true);
 
-	public static class ReverseComparator extends WritableComparator {
-		private static final Text.Comparator TEXT_COMPARATOR = new Text.Comparator();
+        Job job2 = Job.getInstance(conf, "word count 2");
+        job2.setMapperClass(TokenizerMapper.class);
+        job2.setJarByClass(TopkCommonWords.class);
+        job2.setReducerClass(IntSumReducer.class);
+        job2.setOutputKeyClass(Text.class);
+        job2.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job2, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job2, new Path("temp_output2"));
+        job2.waitForCompletion(true);
 
-		public ReverseComparator() {
-			super(Text.class);
-		}
+        Job job3 = new Job(conf, "count common wods");
+        job3.setJarByClass(TopkCommonWords.class);
+        job3.setReducerClass(CommonWordsCount.class);
+        MultipleInputs.addInputPath(job3, new Path("temp_output1"), KeyValueTextInputFormat.class, Mapper1.class);
+        MultipleInputs.addInputPath(job3, new Path("temp_output2"), KeyValueTextInputFormat.class, Mapper2.class);
+        job3.setMapOutputKeyClass(Text.class);
+        job3.setMapOutputValueClass(Text.class);
+        job3.setOutputKeyClass(Text.class);
+        job3.setOutputValueClass(IntWritable.class);
+        job3.setNumReduceTasks(1);
+        FileOutputFormat.setOutputPath(job3, new Path("temp_output3"));
+        job3.waitForCompletion(true);
+        fs.delete(new Path("temp_output1"),true);
+        fs.delete(new Path("temp_output2"),true);
 
-		@Override
-		public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
-			return (-1)* TEXT_COMPARATOR.compare(b1, s1, l1, b2, s2, l2);
-		}
-
-		@SuppressWarnings("rawtypes")
-		@Override
-		public int compare(WritableComparable a, WritableComparable b) {
-			if (a instanceof Text && b instanceof Text) {
-				return (-1)*(((Text) a).compareTo((Text) b));
-			}
-			return super.compare(a, b);
-		}
-	}
-
-
-	public static void main(String[] args) throws Exception {
-		Configuration conf = new Configuration();
-		String[] otherArgs = new GenericOptionsParser(conf, args)
-				.getRemainingArgs();
-
-		if (otherArgs.length != 6) {
-			System.err
-					.println("Usage: CommonWords <input1> <output1> <input2> <output2> "
-							+ "<output3> <output4>");
-			System.exit(2);
-		}
-
-		Job job1 = new Job(conf, "WordCount1");
-		job1.setJarByClass(CommonWords.class);
-		job1.setMapperClass(TokenizerWCMapper.class);
-		job1.setCombinerClass(IntSumReducer.class);
-		job1.setReducerClass(IntSumReducer.class);
-		job1.setOutputKeyClass(Text.class);
-		job1.setOutputValueClass(IntWritable.class);
-		job1.setNumReduceTasks(1);
-		FileInputFormat.addInputPath(job1, new Path(otherArgs[0]));
-		FileOutputFormat.setOutputPath(job1, new Path(otherArgs[1]));
-		job1.waitForCompletion(true);
-
-		Job job2 = new Job(conf, "WordCount2");
-		job2.setJarByClass(CommonWords.class);
-		job2.setMapperClass(TokenizerWCMapper.class);
-		job2.setCombinerClass(IntSumReducer.class);
-		job2.setReducerClass(IntSumReducer.class);
-		job2.setOutputKeyClass(Text.class);
-		job2.setOutputValueClass(IntWritable.class);
-		job2.setNumReduceTasks(1);
-		FileInputFormat.addInputPath(job2, new Path(otherArgs[2]));
-		FileOutputFormat.setOutputPath(job2, new Path(otherArgs[3]));
-		job2.waitForCompletion(true);
-
-		Job job3 = new Job(conf, "Count words in common");
-		job3.setJarByClass(CommonWords.class);
-		job3.setReducerClass(CountCommonReducer.class);
-		MultipleInputs.addInputPath(job3, new Path(otherArgs[1]),
-				KeyValueTextInputFormat.class, Mapper1.class);
-		MultipleInputs.addInputPath(job3, new Path(otherArgs[3]),
-				KeyValueTextInputFormat.class, Mapper2.class);
-
-		job3.setMapOutputKeyClass(Text.class);
-		job3.setMapOutputValueClass(Text.class);
-		job3.setOutputKeyClass(Text.class);
-		job3.setOutputValueClass(IntWritable.class);
-		job3.setNumReduceTasks(1);
-		FileOutputFormat.setOutputPath(job3, new Path(otherArgs[4]));
-		job3.waitForCompletion(true);
-
-		Job job4 = new Job(conf, "sort");
-		job4.setJarByClass(CommonWords.class);
-		job4.setInputFormatClass(KeyValueTextInputFormat.class);
-		job4.setMapperClass(SortMapper.class);
-		job4.setSortComparatorClass(ReverseComparator.class);
-		job4.setReducerClass(SortReducer.class);
-		job4.setOutputKeyClass(IntWritable.class);
-		job4.setOutputValueClass(Text.class);
-		job4.setMapOutputKeyClass(IntWritable.class);
-		job4.setMapOutputValueClass(Text.class);
-		job4.setNumReduceTasks(1);
-		FileInputFormat.addInputPath(job4, new Path(otherArgs[4]));
-		FileOutputFormat.setOutputPath(job4, new Path(otherArgs[5]));
-		System.exit(job4.waitForCompletion(true) ? 0 : 1);
-	}
-
+        Job job4 = new Job(conf, "sort");
+        job4.setJarByClass(TopkCommonWords.class);
+        job4.setInputFormatClass(KeyValueTextInputFormat.class);
+        job4.setMapperClass(SortMapper.class);
+        job4.setSortComparatorClass(IntKeyDescComparator.class);
+        job4.setReducerClass(SortReducer.class);
+        job4.setOutputKeyClass(IntWritable.class);
+        job4.setOutputValueClass(Text.class);
+        job4.setMapOutputKeyClass(IntWritable.class);
+        job4.setMapOutputValueClass(Text.class);
+        job4.setNumReduceTasks(1);
+        FileInputFormat.addInputPath(job4, new Path("temp_output3"));
+        FileOutputFormat.setOutputPath(job4, new Path(args[3]));
+        job4.waitForCompletion(true);
+        fs.delete(new Path("temp_output3"),true);
+        System.exit(0);
+    }
 }
